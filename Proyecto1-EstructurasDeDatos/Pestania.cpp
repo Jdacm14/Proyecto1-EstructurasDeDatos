4bea0dd61e75497355ab678b1218274014989002
Jdacm14 <146151033+Jdacm14@users.noreply.github.com>
2024-09-29 22:05:10 -0600

    Update Pestania.cpp

--- Proyecto1-EstructurasDeDatos/Pestania.cpp
+++ Proyecto1-EstructurasDeDatos/Pestania.cpp
@@ -2,20 +2,15 @@
 
 Pestania::Pestania(): incognito(false)
 {
-	historial = new HistorialNavegacion();
+	historial = HistorialNavegacion();
 }
 
-Pestania::~Pestania()
-{
-	delete historial;
-}
-
-HistorialNavegacion* Pestania::getHistorial()
+HistorialNavegacion& Pestania::getHistorial()
 {
 	return historial;
 }
 
-void Pestania::setHistorial(HistorialNavegacion* histo)
+void Pestania::setHistorial(HistorialNavegacion histo)
 {
 	historial = histo;
 }
@@ -25,7 +20,7 @@ std::vector<Bookmark>& Pestania::geVectortBookmarks()
 	return bookmarks;
 }
 
-void Pestania::agregarBookmark(Bookmark& bo)
+void Pestania::agregarBookmark(Bookmark bo)
 {
 	bookmarks.push_back(bo);
 }
@@ -57,72 +52,22 @@ bool Pestania::getEstadoIncognito()
 
 void Pestania::limpiarHistorialVentana()
 {
-	if (historial) {  // Verifica que el puntero no sea nulo
-		historial->limpiarHistorial();  // Accede al método usando '->' ya que es un puntero
-	}
+	historial.limpiarHistorial();
 }
 
 bool Pestania::siguientePag()
 {
-	return historial && historial->puedeAvanzar();
+		if (historial.puedeAvanzar()) {
+			return true;
+		}
+		return false;
 }
 
 bool Pestania::anteriorPag()
 {
-	return historial && historial->puedeRetroceder();
+	if (historial.puedeRetroceder()) {
+		return true;
+	}
+	return false;
 }
-
-void Pestania::guardarEnBinario(const std::string& nombreArchivo) const {
-	// Abrir el archivo en modo binario
-	std::ofstream archivo(nombreArchivo, std::ios::binary);
-
-	if (!archivo.is_open()) {
-		std::cerr << "Error al abrir el archivo para guardar el historial." << std::endl;
-		return;
-	}
-
-	// Guardar el historial de navegación usando el método de HistorialNavegacion
-	if (historial != nullptr) {
-		historial->guardarEnBinario(nombreArchivo);  // Delegar la tarea a HistorialNavegacion
-	}
-	else {
-		std::cerr << "No hay historial disponible para guardar." << std::endl;
-	}
-
-	// Cerrar el archivo después de guardar los datos
-	archivo.close();
-}
-
-
-void Pestania::cargarDesdeBinario(const std::string& nombreArchivo) {
-	std::ifstream archivo(nombreArchivo, std::ios::binary);
-
-	if (!archivo.is_open()) {
-		std::cerr << "Error al abrir el archivo para cargar los datos de la pestaña." << std::endl;
-		return;
-	}
-
-	// Cargar el estado de incógnito
-	archivo.read(reinterpret_cast<char*>(&incognito), sizeof(incognito));
-
-	// Cargar el historial de navegación
-	historial->cargarDesdeBinario(nombreArchivo);
-
-	// Cargar el número de bookmarks
-	size_t numBookmarks;
-	archivo.read(reinterpret_cast<char*>(&numBookmarks), sizeof(numBookmarks));
-	bookmarks.clear();
-	for (size_t i = 0; i < numBookmarks; ++i) {
-		Bookmark bookmark("", "");
-		bookmark.cargarDesdeBinario(archivo);
-		bookmarks.push_back(bookmark);
-	}
-
-	archivo.close();
-}
-
-
-
-
-
 
