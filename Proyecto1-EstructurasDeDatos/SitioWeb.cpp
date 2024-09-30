7040c44dfa7e10f67a5f4e20969b5901e03c2ccc
Jdacm14 <146151033+Jdacm14@users.noreply.github.com>
2024-09-30 01:12:10 -0600

    Update SitioWeb.cpp

--- Proyecto1-EstructurasDeDatos/SitioWeb.cpp
+++ Proyecto1-EstructurasDeDatos/SitioWeb.cpp
@@ -1,7 +1,7 @@
 #include "SitioWeb.h"
 
 // Constructor
-SitioWeb::SitioWeb(const std::string& _url, const std::string& _titulo) : url(_url), titulo(_titulo), creacion(Clock::now()), tiempo(5){}
+SitioWeb::SitioWeb(const std::string& _url, const std::string& _titulo, const std::string& dom) : url(_url), titulo(_titulo), dominio(dom), creacion(Clock::now()), tiempo(5) {}
 
 // Getters
 std::string SitioWeb::getUrl() const {
@@ -27,6 +27,11 @@ void SitioWeb::setTiempo(int t)
     this->tiempo = t;
 }
 
+void SitioWeb::setCreacion(TimePoint t)
+{
+    creacion = t;
+}
+
 bool SitioWeb::haExpirado() const
 {
     auto ahora = std::chrono::steady_clock::now();
@@ -47,3 +52,45 @@ void SitioWeb::setTitulo(const std::string& _titulo) {
 void SitioWeb::mostrarInfo() const {
     std::cout << "URL: " << url << ", Título: " << titulo << std::endl;
 }
+
+void SitioWeb::guardarArchivoSitioWeb(std::ofstream& out)
+{
+    size_t urlLength = url.length();
+    out.write(reinterpret_cast<const char*>(&urlLength), sizeof(urlLength));
+    out.write(url.c_str(), urlLength);
+
+    size_t tituloLength = titulo.length();
+    out.write(reinterpret_cast<const char*>(&tituloLength), sizeof(tituloLength));
+    out.write(titulo.c_str(), tituloLength);
+
+    size_t dominioLength = dominio.length();
+    out.write(reinterpret_cast<const char*>(&dominioLength), sizeof(dominioLength));
+    out.write(dominio.c_str(), dominioLength);
+
+
+}
+
+SitioWeb SitioWeb::cargarArchivoSitioWeb(std::ifstream& in)
+{
+    size_t urlT;
+    size_t _titulo; 
+    size_t _domT;
+
+    in.read(reinterpret_cast<char*>(&urlT), sizeof(urlT));
+    std::string url(urlT, ' ');
+    in.read(&url[0], urlT);
+
+    in.read(reinterpret_cast<char*>(&_titulo), sizeof(_titulo));
+    std::string titulo(_titulo, ' ');
+    in.read(&titulo[0], _titulo);
+
+    in.read(reinterpret_cast<char*>(&_domT), sizeof(_domT));
+    std::string dominio(_domT, ' ');
+    in.read(&dominio[0], _domT);
+
+    SitioWeb sitio(url, titulo, dominio);
+
+    return sitio;
+
+}
+
