f149e299fca8f148b904ab63d402b9a29ecb78f4
Jdacm14 <146151033+Jdacm14@users.noreply.github.com>
2024-09-29 22:09:02 -0600

    Update SitioWeb.h

--- Proyecto1-EstructurasDeDatos/SitioWeb.h
+++ Proyecto1-EstructurasDeDatos/SitioWeb.h
@@ -4,7 +4,6 @@
 #include<chrono>    //para el temporalizador de los sitios
 #include <stdexcept>    // manejo de excepciones
 #include <set> // para verificar duplicados
-#include<fstream>
 #include<thread>
 #include<memory>
 
@@ -14,17 +13,15 @@ using TimePoint = std::chrono::time_point<Clock>;
 class SitioWeb {
 private:
     std::string url;    // Atributo para la URL
-    std::string dominio; // Atributo para el dominio
     std::string titulo; // Atributo para el título
     TimePoint creacion;
     int tiempo;
 public:
     // Constructor
-    SitioWeb(const std::string& _url = "", const std::string& _dominio = "", const std::string& _titulo = "");
+    SitioWeb(const std::string& _url = "", const std::string& _titulo = "");
 
     // Getters
     std::string getUrl() const;
-    std::string getDominio() const;
     std::string getTitulo() const;
     TimePoint getCreacion() const;
     int getTiempo() const;
@@ -34,15 +31,9 @@ public:
 
     // Setters
     void setUrl(const std::string& _url);
-    void setDominio(const std::string& _dominio);
     void setTitulo(const std::string& _titulo);
-
-    // Guardar
-    void guardarSitio(std::ofstream& archivo) const;
-
-    //Cargar
-    void cargarSitio(std::ifstream& archivo);
 
     // Mostrar información del sitio
     void mostrarInfo() const;
 };
+
